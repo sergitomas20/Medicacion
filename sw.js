ec1211539373cc6387723182f6bde5296ae31c62
sergitomas20 <sergi.tomas20@gmail.com>
2026-08-24 18:01:29 +0200

    Actualizar caché PWA a v1.0.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mi-pauta-v1.0.0";
+const CACHE = "mi-pauta-v1.0.1";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg", "./icon-192.png", "./apple-touch-icon.png"];
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));

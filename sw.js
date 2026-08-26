3b3791442ba143d0a979d9d2ed278094a8b79c45
sergitomas20 <sergi.tomas20@gmail.com>
2026-08-26 14:07:42 +0200

    Actualizar cache para modelo PK

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mi-pauta-v2.2.0';
-const CORE=['./','./index.html','./styles.css?v=2.1.0','./app.js?v=2.1.0','./manifest.webmanifest','./icon.svg','./icon-192.png','./apple-touch-icon.png'];
+const CORE=['./','./index.html','./styles.css?v=2.2.0','./app.js?v=2.2.0','./pk-model.js?v=2.2.0','./manifest.webmanifest','./icon.svg','./icon-192.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

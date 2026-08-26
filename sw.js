c1e8921ccc57d7bd044728f07c4b94fe7698f30a
sergitomas20 <actions@users.noreply.github.com>
2026-08-26 12:07:12 +0000

    Activar modelo PK de tolerancia [skip ci]

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='mi-pauta-v2.1.0';
+const CACHE='mi-pauta-v2.2.0';
 const CORE=['./','./index.html','./styles.css?v=2.1.0','./app.js?v=2.1.0','./manifest.webmanifest','./icon.svg','./icon-192.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
